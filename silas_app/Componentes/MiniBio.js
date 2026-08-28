1312bdec5c7dc9ad3f215dd513d70fc83958b100
Silas Freitas <silas.freitas2525@gmail.com>
2026-08-28 00:08:53 +0000

    comit

---
+++ silas_app/Componentes/MiniBio.js
@@ -0,0 +1,11 @@
+import Profile from '/Profile'
+
+export default function MiniBio(){
+    return (
+        <div className = 'bio'>
+            <Profile/>
+            <h2>Silas Freitas</h2>
+            <p>Sou estudante de t.i e design, além de ser um bom comunicador sou criativo e esforçado.</p>
+        </div>
+    )
+}
---
+++ silas_app/Componentes/Profile.js
@@ -0,0 +1,15 @@
+export default function Profile(){
+    return(
+        <img 
+        scr=""
+        alt=""
+        style={{
+            width:'400px',
+            height:'400px',
+            marginBotton:'20px',
+            borderRadius:'50%',
+            objectFit:'cover'
+        }}
+        />
+    )
+}
